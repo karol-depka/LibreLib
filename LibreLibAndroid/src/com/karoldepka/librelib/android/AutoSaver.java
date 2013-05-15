358622f1fb36bc287d844a34db8b7e43d73352b7
Karol Depka <karolrvn@gmail.com>
2013-05-15 14:15:34 +0200

    Added AutoSaver class

---
+++ LibreLibAndroid/src/com/karoldepka/librelib/android/AutoSaver.java
@@ -0,0 +1,61 @@
+package com.karoldepka.librelib.android;
+
+import android.os.Handler;
+
+
+/** Save occurs: N milliseconds after counter restart.
+ * After save the counter stops.
+ * The counter starts after modification.
+ * If the counter is already running, it will not start again (will run its course to the end).
+ */
+public abstract class AutoSaver {
+	public boolean saveTimerRunning = false;
+
+	public class RunnableSave implements Runnable {
+		@Override public void run() {
+			saveNowIfNeeded();
+			saveTimerRunning = false;
+		}
+	}
+
+	private final int delayMs;
+	private final Handler mHandler = new Handler();
+	private Runnable runnableSave = new RunnableSave();
+
+	public AutoSaver(int delayMs) {
+		this.delayMs = delayMs;
+	}
+
+	public abstract void saveCustom();
+
+	public void documentModified() {
+		ensureSaveTimerRunning();
+	}
+
+	private void ensureSaveTimerRunning() {
+		if ( !saveTimerRunning ) {
+			mHandler.postDelayed(runnableSave, delayMs);
+			saveTimerRunning = true;
+		}
+	}
+
+	public void saveNowIfNeeded() {
+		saveCustom();
+		stopSaveTimer();
+	}
+
+	public void forceSaveNow() {
+		saveNowIfNeeded();
+	}
+	
+	private void stopSaveTimer() {
+		mHandler.removeCallbacks(runnableSave);
+		saveTimerRunning = false;
+	}
+
+	public void destroy() {
+		saveNowIfNeeded();
+	}
+
+
+}
